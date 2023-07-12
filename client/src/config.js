9d8ce6101e236cebe503fbab778405090b9720a1
Kundan Kumar <Kundan9811721117@gmail.com>
2023-07-12 11:16:20 +0530

    update files

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = "https://RedPost-k02.netlify.app/";
+let BASE_URL = "https://redpost-k02.netlify.app/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "Development") {
   BASE_URL = "http://localhost:4000/";
 }
