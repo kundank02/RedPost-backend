fd45784eef85f48117a5c4474ddfd394c5cd65cc
Kundan Kumar <Kundan9811721117@gmail.com>
2023-07-12 12:24:55 +0530

    changes

--- client/src/components/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import { isLoggedIn } from "../helpers/authHelper";
-
-const PrivateRoute = ({ children }) => {
-  return isLoggedIn() ? children : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
